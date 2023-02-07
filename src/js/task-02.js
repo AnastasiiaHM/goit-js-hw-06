5256393951dba2c71f722aa8c3e4ea0d998ca89a
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-02-07 01:09:14 +0100

    task 2,3

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,37 @@
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.querySelector("#ingredients");
+console.log(ulEl);
+
+const liItemEl = document.createElement("li");
+liItemEl.classList.add("item");
+liItemEl.textContent = ingredients[0];
+
+const liItemEl2 = document.createElement("li");
+liItemEl2.classList.add("item");
+liItemEl2.textContent = ingredients[1];
+
+const liItemEl3 = document.createElement("li");
+liItemEl3.classList.add("item");
+liItemEl3.textContent = ingredients[2];
+
+const liItemEl4 = document.createElement("li");
+liItemEl4.classList.add("item");
+liItemEl4.textContent = ingredients[3];
+
+ulEl.append(liItemEl, liItemEl2, liItemEl3, liItemEl4);
+console.log(ulEl);
--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,14 +1,46 @@
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+const ulEl = document.querySelector(".gallery");
+
+const imageElements = images.map((image) => {
+  const liEl = document.createElement("li");
+  const imgEl = document.createElement("img");
+  imgEl.href = image.url;
+  imgEl.alt = image.alt;
+  liEl.append(imgEl);
+  // return ` <li class="item">${liEl}</li>`;
+  return liEl;
+});
+console.log(imageElements);
+ulEl.append(...imageElements);
+
+// const list = document.querySelector(".list");
+
+// const newTechnologies = ["React", "TypeScript", "Node.js"];
+// const markup = newTechnologies
+//   .map((technology) => `<li class="list-item new">${technology}</li>`)
+//   .join("");
+
+// list.insertAdjacentHTML("beforeend", markup);
+// list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
+
+// ulEl.insertAdjacentHTML("afterbegin", ...imageElements);
+
+console.log(ulEl);
